372b03fc00a157a24d233b51c70e9a2dc98a033f
Viet Le <minhviet810business@gmail.com>
2021-11-08 23:20:22 +0800

    add about us

---
+++ pages/about.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+
+<html lang="en">
+	<?php
+		require 'shared/head.php';
+	?>
+	<link rel="stylesheet" href="../css/home.css" />
+	
+	
+	<body>
+	<style>
+	.about_header{
+		text-shadow: 2px 2px 4px #000000;
+		
+	}
+	.about_content{
+		font-family: "Courier New", monospace;
+		font-size: 1.2rem;
+		text-shadow: 2px 2px 4px #000000;
+		
+	}
+	.about_bg {
+		display: flex;
+		justify-content: center;
+		flex-direction: column;
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+		color: white;
+		z-index: 100;
+		background-position: center;
+		background-size: 1100px 720px;
+		height: 720px;
+		width: 1100px;
+		background-repeat: no-repeat;
+		font-size: 25px;
+		border-radius: 25px;
+		padding:5%;
+		/* box-shadow: 5px 5px 5px black; */
+	}
+	.blur_bg {
+		/* filter: brightness(50%); */
+		filter: blur(8px);
+		-webkit-filter: blur(8px) brightness(0.5);
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+		color: white;
+		z-index: 99;
+		background-position: center;
+		background-image: url("..//imgs/banner_bg.jpeg");
+		background-size: 1100px 720px;
+		height: 720px;
+		width: 1100px;
+		background-repeat: no-repeat;
+		font-size: 25px;
+		border-radius: 25px;
+		padding:5%;
+		background-color: rgba(54, 54, 54, 0.5);
+	}
+</style>
+		<?php
+			require 'shared/nav_bar.php';
+
+            if(isset($_SESSION["message"])) {
+                $message = $_SESSION["message"];
+                echo "<script>alert('$message')</script>";
+				unset($_SESSION["message"]);
+            }
+        ?>
+		<div class="banner_container">
+			<div class="banner_bg">
+				<img src="../imgs/banner_bg.jpeg" alt="Snow" style="height: 100%; width: 100%" />
+			</div>
+			
+			<div class="blur_bg">
+				<p></p>
+			</div>
+
+			<div class="about_bg">							
+					<h2 class="about_header">About us</h2><br>
+					<p class="about_content">Our mission is to make cinema bookings easy and accessible for everyone whom have internet access. 
+					<br><br>Cinema bookings be this easy!</p><br>	
+				<h2 class="about_header">Contact us</h2><br>
+					<p class="about_content">Telephone: 666-999-1111<br><br>Email: <a href="mailto:popcorn@gmail.com"><em>popcorn@gmail.com</em></a></p>						
+			</div>
+			
+			
+		</div>
+	</body>
+</html>
